04d6cd0d8b0abefa1bb0add6580a3d46533e89da
Wendel <wendelandradd13@gmail.com>
2026-06-27 01:31:26 -0300

    Começando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        pathMatch: "full",
+        loadComponent: () => {
+            return import("./pages/home/home.component")
+            .then(c => c.HomeComponent)
+        },
+    },
+    
+    {
+        path: "login",
+        pathMatch: "full",
+        loadComponent: () => {
+            return import("./pages/login/login.component")
+            .then(c => c.LoginComponent)
+        }
+    },
+
+];
