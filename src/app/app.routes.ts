74975818f5641e671dd606a90f0831bffd31f2d7
Wendel <wendelandradd13@gmail.com>
2026-06-30 21:13:26 -0300

    Em construção

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,18 +7,18 @@ export const routes: Routes = [
         path: "",
         pathMatch: "full",
         loadComponent: () => {
+            return import("./pages/login/login.component")
+            .then(c => c.LoginComponent)
+        },
+    },
+     
+    {
+        path: "home",
+        pathMatch: "full",
+        loadComponent: () => {
             return import("./pages/home/home.component")
             .then(c => c.HomeComponent)
         },
-    },
-    
-    {
-        path: "login",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages/login/login.component")
-            .then(c => c.LoginComponent)
-        }
     },
 
 ];
