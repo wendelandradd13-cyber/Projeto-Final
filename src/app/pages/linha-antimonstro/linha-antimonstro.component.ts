c094e00d5f03b1c11d49d646a34b61541beeb5dc
Wendel <wendelandradd13@gmail.com>
2026-07-08 00:24:26 -0300

    fazendo

--- src/app/pages/linha-antimonstro/linha-antimonstro.component.ts
+++ src/app/pages/linha-antimonstro/linha-antimonstro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RodapeComponent } from '../../componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-linha-antimonstro',
-  imports: [RouterLink],
+  imports: [RouterLink, RodapeComponent],
   templateUrl: './linha-antimonstro.component.html',
   styleUrl: './linha-antimonstro.component.css'
 })
